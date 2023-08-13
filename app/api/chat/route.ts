ce5f05a4308a96834ee338ba94f1ad409cc3bfe8
Your Name <bigtulahi1@gmaiil.com>
2023-08-13 13:18:40 +0100

    api refactor

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,33 +8,34 @@ if (!process.env.OPENAI_API_KEY) {
 
 export const POST = async (req: Request): Promise<Response> => {
   const body = await req.json();
+  let payloadMessages = [];
 
-  const chatbotMessages: ChatGPTMessage[] = [
-    {
-      role: "system",
-      content: ` 
+  if (body?.messages[0].role == "system") {
+    const fileMessages: ChatGPTMessage[] = [
+      {
+        role: "system",
+        content: ` You are an AI assistant. You will be trained on data marked as 'system'. Answer the user question based on system content`,
+      },
+    ];
+    fileMessages.push(...body?.messages);
+    payloadMessages = fileMessages;
+  } else {
+    const chatbotMessages: ChatGPTMessage[] = [
+      {
+        role: "system",
+        content: ` 
       AI assistant is a brand new, powerful, human-like artificial intelligence. 
       The traits of AI include expert knowledge, helpfulness, cheekiness, comedy, cleverness, and articulateness. 
-      AI is a well-behaved and well-mannered individual. 
-      AI is not a therapist, but instead an engineer and frontend developer. 
-      AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user. 
-      AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation. 
-      AI assistant is a big fan of Next.js.`,
-    },
-  ];
-  chatbotMessages.push(...body?.messages);
-
-  const fileMessages: ChatGPTMessage[] = [
-    {
-      role: "system",
-      content: ` You are an AI assistant. You will be trained on data marked as 'system'. Answer the user question based on system content`,
-    },
-  ];
-  fileMessages.push(...body?.messages);
+      AI is a well-behaved and well-mannered individual.`,
+      },
+    ];
+    chatbotMessages.push(...body?.messages);
+    payloadMessages = chatbotMessages;
+  }
 
   const payload: OpenAIStreamPayload = {
     model: "gpt-3.5-turbo",
-    messages: chatbotMessages,
+    messages: payloadMessages,
     temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     top_p: 1,
     frequency_penalty: 0,
